58be64beb44c90cab66d9b1d18c914b8af388fb7
zhangchunyang <zhangchunyang@okjiaoyu.cn>
2017-07-20 19:53:12 +0800

    travis ci 修正

--- fastweb/test/test_loader.py
+++ fastweb/test/test_loader.py
@@ -8,11 +8,11 @@ class TestLoader(object):
 
     def test_load_recorder(self):
         app.load_recorder()
-        app.load_recorder('log/app.log')
-        app.load_recorder('log/app.log', system_log_path='log/sys.log')
-        app.load_recorder('log/app.log', system_log_path='log/sys.log')
-        app.load_recorder('log/app.log', system_log_path='log/sys.log', application_level='INFO')
-        app.load_recorder('log/app.log', system_log_path='log/sys.log', system_level='INFO')
+        app.load_recorder('app.log')
+        app.load_recorder('app.log', system_log_path='sys.log')
+        app.load_recorder('app.log', system_log_path='sys.log')
+        app.load_recorder('app.log', system_log_path='sys.log', application_level='INFO')
+        app.load_recorder('app.log', system_log_path='sys.log', system_level='INFO')
 
     def test_load_configuration(self):
         app.load_configuration(backend='ini', path='config/config.ini')
