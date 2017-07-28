8de87c91fb76c8d21a191da0a62262a72439a212
zhangchunyang <zhangchunyang@okjiaoyu.cn>
2017-07-28 16:26:03 +0800

    测试travis ci

---
+++ fastweb/test/service_server.py
@@ -0,0 +1,10 @@
+# coding:utf8
+
+from fastweb.loader import app
+from fastweb.service import start_service_server
+
+
+if __name__ == '__main__':
+    app.load_recorder('service.log', system_level='DEBUG')
+    app.load_component(layout='service', backend='ini', path='config/service.ini')
+    start_service_server()
