30104e31bebc4750d43300dee2d59c498471dce5
zhangchunyang <zhangchunyang@okjiaoyu.cn>
2017-07-28 16:47:13 +0800

    测试travis ci

--- fastweb/test/test_manager.py
+++ fastweb/test/test_manager.py
@@ -7,21 +7,21 @@ from fastweb.manager import Manager, SyncConnManager, AsynConnManager
 class TestManager(object):
 
     def test_setup(self):
-        configer = app.load_component('web', backend='ini', path='config/service.ini')
+        configer = app.load_component('web', backend='ini', path='fastweb/test/config/service.ini')
         Manager.setup(configer)
 
 
 class TestSyncManager(object):
 
     def test_setup(self):
-        configer = app.load_component('web', backend='ini', path='config/service.ini')
+        configer = app.load_component('web', backend='ini', path='fastweb/test/config/service.ini')
         SyncConnManager.setup(configer)
 
 
 class TestAsynManager(object):
 
     def test_setup(self):
-        configer = app.load_component('web', backend='ini', path='config/service.ini')
+        configer = app.load_component('web', backend='ini', path='fastweb/test/config/service.ini')
         AsynConnManager.setup(configer)
 
 
