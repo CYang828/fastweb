528845288be213945c070354956296f254ed0bdb
zhangchunyang <zhangchunyang@okjiaoyu.cn>
2017-12-07 11:04:31 +0800

    queue 队列模块2和3的兼容

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from setuptools import setup, find_packages
 
 kwargs = {}
 install_requires = []
-version = '1.5.0.2'
+version = '1.5.0.3'
 
 if sys.version_info < (3, 0):
     with open('README.rst') as f:
