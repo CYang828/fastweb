064c688c6d6c391bb654e2411ef86ae39b74f570
zhangchunyang <zhangchunyang@okjiaoyu.cn>
2017-12-19 17:45:07 +0800

    1.5.0.11

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from setuptools import setup, find_packages
 
 kwargs = {}
 install_requires = []
-version = '1.5.0.10'
+version = '1.5.0.11'
 
 if sys.version_info < (3, 0):
     with open('README.rst') as f:
