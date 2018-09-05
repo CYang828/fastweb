8b9ea90f35c198787679908085cbfc13781a9430
zhangchunyang <zhangchunyang@okjiaoyu.cn>
2018-09-05 10:48:31 +0800

    Version:1.6.0.4     add：增加flower监控     add: 增加异步cache装饰器，注意如果被装饰的函数需要用的coroutine，需要最后使用coroutine装饰器

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from setuptools import setup, find_packages
 
 kwargs = {}
 install_requires = []
-version = '1.6.0.4'
+version = '1.6.0.5'
 
 if sys.version_info < (3, 0):
     with open('README.rst') as f:
