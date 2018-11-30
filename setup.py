8b2a76599af82e3ebf52818ee398334f974fc810
zhangchunyang <zhangchunyang@okjiaoyu.cn>
2018-12-01 00:57:06 +0800

    delete page web argument body

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from setuptools import setup, find_packages
 
 kwargs = {}
 install_requires = []
-version = '1.6.0.11'
+version = '1.6.0.12'
 
 if sys.version_info < (3, 0):
     with open('README.md') as f:
