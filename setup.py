78e6082eb25072e09baf9702acaf54e26314956d
zhangchunyang <zhangchunyang@okjiaoyu.cn>
2019-02-13 18:14:10 +0800

    1.6.0.14 修复str2everything方法漏洞

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ from setuptools import setup, find_packages
 
 kwargs = {}
 install_requires = []
-version = '1.6.0.15'
+version = '1.6.0.16'
 
 if sys.version_info < (3, 0):
     with open('README.md') as f:
